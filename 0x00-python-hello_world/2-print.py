e4baa18b8c24fc9de10483229a45d41c99512a74
Hezekiah Osuolale <101516900+osuolaledeveloper@users.noreply.github.com>
2022-07-21 19:51:30 +0100

    Update 2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-
-# This program is to print  some string on the terminal
-# It is my first python code 
-
 print("\"Programming is like building a multilingual puzzle")
-
-# The back slash is the escape keyword
-#it allow me to use quote inside another quote
