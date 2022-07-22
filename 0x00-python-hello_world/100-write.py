c21867e8d046e772bdbef2240ca1daf1a1eddf93
osuolaledeveloper <osuoalehezekiah@gmail.com>
2022-07-22 09:53:47 +0100

    1-6

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.\n")
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
 sys.exit(1)
